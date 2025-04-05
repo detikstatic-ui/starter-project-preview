9e6c266f39441b020294e7535c9081d46dae28ac
Yudis <yudis@Yudis-MacBook-Pro.local>
2025-04-05 09:31:54 +0700

    tailwind variant example

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -30,6 +30,7 @@ module.exports = {
     importOrderMergeDuplicateImports: true,
     importOrderCombineTypeAndValueImports: true,
     plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+    tailwindFunctions: ["tv"],
     overrides: [
         {
             files: "*.astro",
